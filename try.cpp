b5769436e0fc7844e8a5b0e1d1739392016bb4b0
Sankalp Jain <sankalp.jain3998@gmail,com>
2020-04-23 06:21:12 +0530

    Try 2

--- try.cpp
+++ try.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-#define level 3
+#define grade_a 75
+#define grade_b 50
+#define grade_c 25
 
 using namespace std;
 
 
 int main()
 {
-    #ifdef level == 0
-        #define score 0
-    else
-        #ifdef level == 1
-            #define score == 1
-    #endif
-    #endif
-    elif level == 2   
-        #define score == 30
-    elif level == 4
-        #define score 45    
-    endif
+    int val = 0;
+    cout <<"Enter the value of your choice in-between(0-100) :";
+    cin >> val;
 
-    #ifdef score
-        cout<<score;
-    #endif    
+    if(val < grade_c)
+    {
+        cout << "Fail : Grade C";
+    }    
+    else if(val < grade_b)
+    {
+        cout << "Pass: Grade B";
+    }
+    else 
+        cout <<"Excellent : Grade A";
     return 0;
 }
