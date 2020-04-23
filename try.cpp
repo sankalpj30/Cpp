41e6626182f4284aa8c1b7a7095ab74e990fa87a
Sankalp Jain <sankalp.jain3998@gmail,com>
2020-04-23 06:44:30 +0530

    try 3

--- try.cpp
+++ try.cpp
@@ -9,18 +9,25 @@ using namespace std;
 int main()
 {
     int val = 0;
+    int age;
+    string name;
+    cout <<"Enter your name : ";
+    getline(cin,name);
+    cout << "Enter your age : ";
+    cin>> age;
     cout <<"Enter the value of your choice in-between(0-100) :";
     cin >> val;
-
+    cout << name ;
+    cout <<("\n",age);
     if(val < grade_c)
     {
-        cout << "Fail : Grade C";
+        cout << "\nFail : Grade C";
     }    
     else if(val < grade_b)
     {
-        cout << "Pass: Grade B";
+        cout << "\nPass: Grade B";
     }
     else 
-        cout <<"Excellent : Grade A";
+        cout <<"\nExcellent : Grade A";
     return 0;
 }
