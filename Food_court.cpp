801580a3e5644c86924fa4f3000aec222406faf5
Sankalp Jain <sankalp.jain3998@gmail,com>
2020-04-25 08:54:31 +0530

    Whe have used do while loop to make our code work more efficiently

--- Food_court.cpp
+++ Food_court.cpp
@@ -6,11 +6,14 @@ using namespace std;
 int main()
 {
     string nm;
-    int num;   
+    int num; 
     cout << "Please enter your name : ";
+    //cin.ignore();
     getline(cin, nm);
-    cout << "Hello, "<<nm<< " What would you like to have : ";
-    cout << "\nMenu \n1.Fries\n2.Burger\n3.Pizza\n";
+    cout << "Hello, "<< nm << " What would you like to have : ";
+   do
+   { 
+    cout << "\nMenu \n1.Fries\n2.Burger\n3.Pizza\n4.Exit\n";
     cout << "Please enter the number regarding your choice : ";
     cin >> num;
     switch(num){
@@ -23,6 +26,15 @@ int main()
         case 3 :         
                 cout << "Price for pizza is 10.";
                 break;
+        case 4 :
+                exit (0);        
+        default : 
+        {
+                cout << "You have entered wrong input";
+        }
     }
+   }
+    while(num!=4); 
     return 0;
+
 }
